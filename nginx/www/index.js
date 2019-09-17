888d8736c882a82b669c848598ee5a68b2a19f64
Jørund Leknes <jorund.leknes@gmail.com>
2019-09-17 17:25:19 +0200

    Gått bort fra Docker og over på python web server

--- nginx/www/index.js
+++
@@ -1,7 +0,0 @@
-let static = require('node-static');
-let file = new static.Server();
-require('http').createServer(function(request, response) {
-    request.addListener('end', function() {
-        file.serve(request, response);
-    }).resume();
-}).listen(process.env.PORT || 3000);
